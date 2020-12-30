727f079734f87a868a6b89cb17da4bf0971bd3eb
Ankit Malik <ankit.malik1092@gmail.com>
2020-12-31 02:01:34 +0530

    add components

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+//import ReactDom from "react-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            {notes.map( noteItem => 
+                <Note
+                    key = {noteItem.id}
+                    title = {noteItem.title}
+                    content = {noteItem.content} 
+                /> 
+            )}
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/notes.js
@@ -0,0 +1,14 @@
+const notes = [
+    {
+        id: 0,
+        title: "Learn React",
+        content: "It's almost complete but there's still a lot to learn."
+    },
+    {
+        id: 1,
+        title: "Complete JAVA tests",
+        content: "Only 9 tests (30%) completed!"
+    }
+]
+
+export default notes;
