735a3465f3af1fd837419cc4aaebce0947211c19
Ankit Malik <ankit.malik1092@gmail.com>
2021-02-04 22:40:20 +0530

    start google oauth

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import HighlightIcon from "@material-ui/icons/Highlight";
+import GoogleLogin from 'react-google-login';
+
+const responseGoogle = (response) => {
+    console.log(response);
+}
 
 const Header = () => {
     return (
@@ -8,6 +13,13 @@ const Header = () => {
                 <HighlightIcon />
                 Keeper
             </h1>
+            <div><GoogleLogin
+                clientId= "927001483726-5vuuseba1irmvncv1a7g6io7baje9ic1.apps.googleusercontent.com"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            /></div>
         </header>
     );
 }
