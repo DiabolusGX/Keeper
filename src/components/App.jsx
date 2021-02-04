d0b7f3e9d34cd2b2236ba2e848004b186d40f92e
Ankit Malik <ankit.malik1092@gmail.com>
2021-02-04 19:36:14 +0530

    added material-ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,42 @@
 import React from "react";
 //import ReactDom from "react-dom";
 import Header from "./Header";
+import CreateArea from "./CreateArea";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+//import notes from "../notes";
 
 const App = () => {
+
+    const [notes, setNotes] = React.useState([]);
+
+    function addNote(note) {
+        setNotes(prevNotes => {
+            return [ ...prevNotes, note ];
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
     return (
         <div>
             <Header />
-            {notes.map( noteItem => 
+            <CreateArea 
+                onAdd = {addNote}
+            />
+            {notes.map( (noteItem, index) => 
                 <Note
-                    key = {noteItem.id}
+                    key = {index}
+                    id = {index}
                     title = {noteItem.title}
-                    content = {noteItem.content} 
+                    content = {noteItem.content}
+                    onDelete = {deleteNote}
                 /> 
             )}
             <Footer />
